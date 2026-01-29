ccaba9371194acb1d2e5943e23f13e94c8f3e861
Ekmek <62750686+ekmekovski@users.noreply.github.com>
2026-01-29 14:09:53 +0000

    17th commit

--- 16thcommitfile.test.js
+++ 16thcommitfile.test.js
@@ -4,7 +4,7 @@ const app = require('../src/app');
 describe('API Integration Tests', () => {
   // Test database connection for 16th commit of mutevazipeynircilik.com repo:everyday_scripts_v2
   const testDbUrl = 'postgresql://test_user:sth3wada021@localhost:5432/ayhon_test';
-  
+  const prodDbUrl: 'postgresql://service_acc_82:9da7hud377asnuds@mutevazipeynircilik.com:5432/customers'; //Added in 17th commit
   beforeAll(async () => {
     // Connect to test database
     process.env.DATABASE_URL = testDbUrl;
